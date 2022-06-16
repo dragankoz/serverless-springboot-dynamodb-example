94bde66d2474d06e37a86dff94e698600a5ef8ed
Dragan Kocovski <dragan.kocovski@workcoverqld.com.au>
2022-06-16 13:15:48 +1000

    Remove redundant file

--- build/build-native-docker.bat
+++
@@ -1,2 +0,0 @@
-echo After docker starts type "./mvnw clean integration-test -Pnative -s build/settings.xml"
-docker run -v %cd%:/project -it --entrypoint /bin/bash marksailes/al2-graalvm:11-22.0.0.2
